cebb9f13fca481d518c6a45bef2cee26966abbfe
xenialindegren <RF06GI@student.aau.dk>
2025-11-24 11:18:06 +0100

    Opdaterede og tilføjede testfiler samt inkluderede dem i CMakeLists så opsætningen er klar til udfyldning

---
+++ test/test_functions.c
@@ -0,0 +1,43 @@
+#include "mtest.h"
+#include "functions.h"
+
+// Test the open file function
+
+//=======tests for data funktion===============
+TEST_CASE(open_data_test, {
+    char file_name[] = "src/Data.txt";
+    int expected = 0; // Arrange
+    FILE* fptr = open_data(file_name); // Act
+    CHECK_TRUE(fptr != NULL); // Assert
+})
+
+
+TEST_CASE(example_fail, {
+    CHECK_FAIL("This test fails, so we can see how a failing test case looks like.");
+})
+
+
+//========tests for get_file_path_from_user funktionen======
+TEST_CASE(get_file_path_from_user_test, {
+    // Arrange
+    // Act
+    // Assert
+})
+
+//======test get_size_of_map funktionen ===============
+TEST_CASE(get_size_of_map_test, {
+
+})
+
+//=======test initialize_array funktionen ============
+TEST_CASE(initialize_array_test, {
+    // Arrange
+
+    // Act
+    // Assert
+})
+
+
+
+// Register your test cases to this macro:
+MAIN_RUN_TESTS(open_data_test, get_file_path_from_user_test, get_size_of_map_test, initialize_array_test)
---
+++ test/test_simulations.c
@@ -0,0 +1,18 @@
+#include "mtest.h"
+#include "simulation.h"
+
+// Test the open file function
+TEST_CASE(test, {
+    // Arrange
+    // Act
+    // Assert
+})
+
+
+TEST_CASE(example_fail, {
+    CHECK_FAIL("This test fails, so we can see how a failing test case looks like.");
+
+})
+
+// Register your test cases to this macro:
+MAIN_RUN_TESTS(test)
